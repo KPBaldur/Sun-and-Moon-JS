5176ca9288f7964ad2196f49ced1a2265155cbf8
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2023-05-01 17:59:32 -0400

    Script de Cambio modo

---
+++ script.js
@@ -0,0 +1,24 @@
+const checkbox =document.querySelector('#checkbox');
+const imagen = document.querySelector('#imagen');
+const texto = document.querySelectorAll('h1');
+
+const colorPrimary = getComputedStyle(document.documentElement).getPropertyValue('--bg-color');
+const SecondarycolorPrimary = getComputedStyle(document.documentElement).getPropertyValue('--bg-secondary-color');
+
+checkbox.onclick = function(){
+    if (checkbox.checked){
+        imagen.src = "img/Luna.png";
+        document.body.style.backgroundColor=  SecondarycolorPrimary;
+        document.body.style.color = "white";
+        texto.forEach(h1 => {
+            h1.style.color = "white";
+        });
+        } else {
+            imagen.src = "img/celestia.png";
+            document.body.style.backgroundColor = colorPrimary;
+            document.body.style.color= "black";
+            texto.forEach(h1 => {
+                h1.style.color = "black";
+            })
+        }
+}
